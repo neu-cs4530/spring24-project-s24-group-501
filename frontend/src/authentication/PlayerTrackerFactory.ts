3a9611347abb32ba2035cb8a25cbef625f6c0cba
darchibald <archibald.d@northeastern.edu>
2024-03-30 19:01:24 -0400

    PlayerTrackerFactory

---
+++ frontend/src/authentication/PlayerTrackerFactory.ts
@@ -0,0 +1,15 @@
+import PlayerTracker from './PlayerTracker';
+
+/**
+ * Returns a singleton instance of the PlayerTracker class.
+ */
+export default class PlayerTrackerFactory {
+  private static _instance: PlayerTracker | undefined;
+
+  public static instance(): PlayerTracker {
+    if (!PlayerTrackerFactory._instance) {
+        PlayerTrackerFactory._instance = new PlayerTracker();
+    }
+    return PlayerTrackerFactory._instance;
+  }
+}
