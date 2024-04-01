895b2e8679f9684ad32d9b48a639f5b7709814f2
aridagan12 <aridagan12@gmail.com>
2024-04-01 13:25:22 -0400

    Animated the map and added it to the scene

--- frontend/src/components/Town/TownGameScene.ts
+++ frontend/src/components/Town/TownGameScene.ts
@@ -9,6 +9,7 @@ import ConversationArea from './interactables/ConversationArea';
 import GameArea from './interactables/GameArea';
 import Transporter from './interactables/Transporter';
 import ViewingArea from './interactables/ViewingArea';
+import CasinoArea from './interactables/CasinoArea';
 
 // Still not sure what the right type is here... "Interactable" doesn't do it
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
