25c5fae076c128dbda96c7cdf93e41379789849a
aridagan12 <aridagan12@gmail.com>
2024-04-08 10:13:56 -0400

    some more fixes

--- frontend/src/classes/interactable/CasinoAreaController.ts
+++ frontend/src/classes/interactable/CasinoAreaController.ts
@@ -96,7 +96,8 @@ export default abstract class CasinoAreaController<
 
   protected _updateFrom(newModel: CasinoArea<State>): void {
     const gameEnding =
-      this._model.game?.state.status === 'IN_PROGRESS' && newModel.game?.state.status !== 'IN_PROGRESS';
+      this._model.game?.state.status === 'IN_PROGRESS' &&
+       newModel.game?.state.status !== 'IN_PROGRESS';
     const newPlayers =
       newModel.game?.players.map(playerID => this._townController.getPlayer(playerID)) ?? [];
     if (!newPlayers && this._players.length > 0) {
